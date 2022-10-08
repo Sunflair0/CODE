0e27ef45a578946397ca21937d353e1c7564b41d
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-10-08 09:42:55 -0500

    close button works

--- dist/js/main.js
+++ dist/js/main.js
@@ -105,26 +105,25 @@ function skillChoice() {
 
 // /////video section
 let video = document.getElementById('video');
-let vidClose = document.getElementbyId("video-close");
+let vidClose = document.getElementsByClassName("video-close");
 let vidPlayer = document.getElementsByClassName("video-player");
 let toggle__fade = document.getElementsByClassName("toggle__fade");
-let btn = document.querySelector("button");
 
 function preview_select() {
     let preview = document.querySelector('[name="preview"]:checked').value;
 
     toggle__fade[0].style.cssText = "color: transparent; text-shadow: none";
-    vidClose.style.cssText = "opacity: 1";
+    vidClose[0].style.cssText = "opacity: 1";
     video.style.cssText = "opacity: 1; display: visible";
     video.src = `/Portfolio2021/dist/videos/${preview}`;
     return
 };
 
-btn.addEventListener("click", videoClose);
+vidClose.addEventListener("click", videoClose);
 function videoClose() {
     video.style.cssText = "opacity: 0; display: hidden";
-    vidClose.style.cssText = "opacity: 0";
-    toggle__fade[0].style.cssText = "color: transparent; text-shadow: -1px -1px 0 rgba(0, 0, 0, 0.7), 1px -1px rgb(0, 0, 0), 2px -1px 0 rgba(27, 27, 27, 0.7), -1px 1px 0 rgba(0, 0, 0, 0.7), 1px 1px 0 rgba(39, 39, 39, 0.7)";
+    vidClose[0].style.cssText = "opacity: 0";
+    toggle__fade[0].style.cssText = "color: #ef7f31; text-shadow: -1px -1px 0 rgba(0, 0, 0, 0.7), 1px -1px rgb(0, 0, 0), 2px -1px 0 rgba(27, 27, 27, 0.7), -1px 1px 0 rgba(0, 0, 0, 0.7), 1px 1px 0 rgba(39, 39, 39, 0.7)";
     return
 };
 
