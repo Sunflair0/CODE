769a0fa6dc9afa5b1db0b090b4676393072ccbd1
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2024-01-03 10:38:29 -0600

    update menu js

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,31 +6,18 @@ const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
-let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+if (menuBtn)
+    menuBtn.addEventListener('click', () => {
+        hamburger.classList.toggle('open')
+        nav.classList.toggle('open');
+        menuNav.classList.toggle('open');
+        navItems.forEach(item => item.classList.toggle('open'));
+    })
 
-function toggleMenu() {
-    if (!showMenu) {
-        hamburger.classList.add('open')
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItems.forEach(item => item.classList.add('open'));
-
-        showMenu = true;
-
-    } else {
-        hamburger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        navItems.forEach(item => item.classList.remove('open'));
-
-        showMenu = false;
-    }
+else {
+    console.log("put the code back");
 }
-
-
-
 
 ///// // scroll fade
 const fadeElements = document.querySelectorAll(".section_content");
